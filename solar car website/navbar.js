ad1753112fe53a41a1bcfe93879c8b75599e167e
small-j1337 <jerryliang.mv@gmail.com>
2025-09-18 18:02:13 -0700

    Universal navbar HTML in navbar.js

--- solar car website/navbar.js
+++ solar car website/navbar.js
@@ -1,4 +1,30 @@
-
+function renderNavbar(){
+  return `<div class = "logoandname">
+            <img src = "images/logo-black-outline.png">
+            <h1><a href = "index.html" style = "text-decoration: none; color: white">GARAGE 803 RACING</a></h1>
+        </div>
+        <button class = "hamburger" id = "hamburger">&#9776</button>
+        <div class = "links" id = "links">
+            <button style = "background: none; border: none; color: white; font-size: 5vw" class = "close" id = "close" onclick = "navLinks.classList.toggle('show'); document.body.classList.toggle('no-scroll')">&times</button>
+            <ul>
+                <li><a href = "index.html">Home</a></li>
+                <li><a class = "team-link" id = "team-link">Team</a>
+                    <ul class = "team-dropdown" id = "team-dropdown">
+                        <li><a href = "team-25.html">2024-2025</a></li>
+                        <li><a href = "team-26.html">2025-2026</a></li>
+                    </ul>
+                </li>
+                <li><a href = "sponsors.html">Sponsors</a></li>
+                <li><a href = "media.html">Media</a></li>
+                <li><a href = "donate.html">Donate</a></li>
+                <li><a href = "faq.html">FAQ</a></li>
+                <button class = "join-navbar-button" onclick = "location.assign('join.html')">Join</button>
+                <button class = "contact-navbar-button" onclick = "location.assign('contact.html')">Contact</button>
+            </ul>
+        </div>`
+        ;
+}
+document.getElementById("navbar").innerHTML = renderNavbar();
     const hamburger = document.getElementById("hamburger");
     const navLinks = document.getElementById("links");
 
